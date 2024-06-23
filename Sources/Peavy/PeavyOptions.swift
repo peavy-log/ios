8a2b74d2e8340d57f935774d40fce677a1a12e98
Magnus <post@magnusmork.no>
2024-06-23 20:51:28 +0200

    fix visibility

--- Sources/Peavy/PeavyOptions.swift
+++ Sources/Peavy/PeavyOptions.swift
@@ -3,5 +3,15 @@ import Foundation
 public struct PeavyOptions {
     let endpoint: URL
     let logLevel: LogLevel
-    let printToStdout: Bool = false
+    let printToStdout: Bool
+    
+    public init(
+        endpoint: URL,
+        logLevel: LogLevel,
+        printToStdout: Bool = false
+    ) {
+        self.endpoint = endpoint
+        self.logLevel = logLevel
+        self.printToStdout = printToStdout
+    }
 }
