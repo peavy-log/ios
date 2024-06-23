32816cd84dd0bbe9924939b4eef804b8c41e9ef0
Magnus <post@magnusmork.no>
2024-06-23 23:37:10 +0200

    options docs

--- Sources/Peavy/PeavyOptions.swift
+++ Sources/Peavy/PeavyOptions.swift
@@ -4,19 +4,56 @@ public struct PeavyOptions {
     let endpoint: URL
     let logLevel: LogLevel
     let enableCrashReporting: Bool
+    let pushInterval: TimeInterval
     let printToStdout: Bool
     let debug: Bool
     
     public init(
+        /**
+         * The remote endpoint to push logs to.
+         * Should be a full URL.
+         */
         endpoint: URL,
+        /**
+         * Minimum log level to process.
+         *
+         * Default: LogLevel.Info
+         */
         logLevel: LogLevel,
+        /**
+         * Whether or not to enable the automatic crash detection and logging.
+         *
+         * Default: true
+         */
         enableCrashReporting: Bool = true,
+        /**
+         * How often to push logs to remote.
+         * Logs are cached locally on device, and sent once every pushInterval.
+         *
+         * Default: 30 seconds
+         */
+        pushInterval: TimeInterval = 30,
+        /**
+         * Enable Peavy to also print the log line to stdout
+         * (using builtin NSLog)
+         *
+         * If Peavy is being used directly from within code
+         * (ie. not through f.ex. Lumberjack), then you probably want
+         * this enabled in debug builds.
+         */
         printToStdout: Bool = false,
+        /**
+         * Whether to enable library debug mode.
+         * This enables logging (to stdout only) of local Peavy actions
+         *
+         * Default: false
+         */
         debug: Bool = false
     ) {
         self.endpoint = endpoint
         self.logLevel = logLevel
         self.enableCrashReporting = enableCrashReporting
+        self.pushInterval = pushInterval
         self.printToStdout = printToStdout
         self.debug = debug
     }
