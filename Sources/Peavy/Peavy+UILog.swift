546267afa7df58e3a414d82a2c160ab0c477442c
Magnus <post@magnusmork.no>
2024-07-06 23:13:09 +0200

    resolve alertaction styles

--- Sources/Peavy/Peavy+UILog.swift
+++ Sources/Peavy/Peavy+UILog.swift
@@ -39,7 +39,16 @@ internal extension UIAlertAction {
                                        style: UIAlertAction.Style,
                                        handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
         return peavyInit(title: title, style: style) { a in
-            Peavy.i("Action: \(a.style) \(a.title ?? "<no title>")")
+            Task {
+                var style: String = ""
+                switch a.style {
+                case .default: style = "default"
+                case .cancel: style = "cancel"
+                case .destructive: style = "destructive"
+                default: style = "\(a.style.rawValue)"
+                }
+                Peavy.i("Action: \(style) \(a.title ?? "<no title>")")
+            }
             handler?(a)
         }
     }
